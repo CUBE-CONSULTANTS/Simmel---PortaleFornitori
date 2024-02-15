7d1774f7c8d4b1516209894df42bd1a5527e8a58
Lulle91 <125670291+Lulle91@users.noreply.github.com>
2024-02-15 17:54:36 +0100

    no factory si fragment

--- portale_fornitori/webapp/controller/Documenti.controller.js
+++ portale_fornitori/webapp/controller/Documenti.controller.js
@@ -1,9 +1,12 @@
 sap.ui.define(
-  ["./BaseController", "sap/ui/model/json/JSONModel", "sap/m/ColumnListItem","sap/m/Button","sap/m/Input","sap/m/Select","sap/m/Text","sap/ui/core/Icon","sap/ui/core/Item"],
+  ["./BaseController", 
+  "sap/ui/model/json/JSONModel", 
+  "sap/ui/core/Fragment"
+],
   /**
    * @param {typeof sap.ui.core.mvc.Controller} Controller
    */
-  function (Controller, JSONModel,ColumnListItem, Button,Input,Select,Text,Icon,Item) {
+  function (Controller, JSONModel,Fragment) {
     "use strict";
 
     return Controller.extend(
@@ -24,6 +27,7 @@ sap.ui.define(
           this.setModel(oModelDocMezzi,"mezziModel")
           let oValDoc = new JSONModel(data.validazione)
           this.setModel(oValDoc, "valModel")
+          this.sKey
         },
         navToRicercaDoc: function () {
           this.getRouter().navTo("RicercaDoc");
@@ -38,154 +42,206 @@ sap.ui.define(
           oEvent.getSource().getParent().getParent().close()
         },
         onIconTabSelect: function (oEvent) {
-          debugger
-          let sKey = oEvent.getSource().getSelectedKey();
-          let oTable = this.byId("changeTable"); 
-          const sTipoUtente = this.getModel("userModel").getProperty("/tipoUtente");
-          const aCellContent = this.createCellContent(sKey, sTipoUtente);
-          const oTemplate = new ColumnListItem({ cells: aCellContent });
-            
-            switch (sKey) {
-                case "info":
-                    oTable.bindItems({
-                        path: "docModel>/",
-                        templateShareable: false,
-                        template: oTemplate
-                    });
-                    break;
-                case "people":
-                    oTable.bindItems({
-                        path: "personaleModel>/",
-                        templateShareable: false,
-                        template: oTemplate
-                    });
-                    break;
-                case "car":
-                    oTable.bindItems({
-                        path: "mezziModel>/",
-                        templateShareable: false,
-                        template: oTemplate
-                    });
-                    break;
-                default:
-                    break;
-            }
-        },
-       createCellContent: function (sModelName, sTipoUtente) {
-        debugger
-          switch (sModelName) {
-              case "info":
-                  return [
-                      new Text({ text: "{docModel>descrizione}" }),
-                      new Button({
-                          visible: "{= !${docModel>scarica} ? false : true}",
-                          icon: "{docModel>scarica}",
-                          type: "Emphasized"
-                      }),
-                      new Select({
-                          width: "150px",
-                          items: { path: "docModel>validazione", templateShareable: false,
-                          template: new Item({
-                            key: "{value}",
-                            text: "{text}"
-                        })
-                        }
-                      }),
-                      new Button({
-                          type: "Emphasized",
-                          icon: "{docModel>allega}",
-                          press: "onOpenMaskAllega"
-                      }),
-                      new Icon({
-                          alt: "Download",
-                          size: "1.3rem",
-                          src: "{docModel>stato}",
-                          color: "{docModel>color}"
-                      }),
-                      new Text({ text: "{docModel>data}" }),
-                      new Text({ text: "{docModel>esito_validazione}" }),
-                      new Input({
-                          textAlign: "Center",
-                          value: "{docModel>scadenza}",
-                          editable: "{= ${userModel>/tipoUtente} === 'Interno' ? false : ${docModel>editable} }"
-                      })
-                  ];
-              case "people":
-                return [
-                  new Text({ text: "{personaleModel>descrizione}" }),
-                  new Button({
-                      visible: "{= !${personaleModel>scarica} ? false : true}",
-                      icon: "{personaleModel>scarica}",
-                      type: "Emphasized"
-                  }),
-                  new Select({
-                      width: "150px",
-                      items: { path: "personaleModel>validazione" , templateShareable: false,
-                    template: new Item({
-                        key: "{value}",
-                        text: "{text}"
-                    })
-                  }
-                  }),
-                  new Button({
-                      type: "Emphasized",
-                      icon: "{personaleModel>allega}",
-                      press: "onOpenMaskAllega"
-                  }),
-                  new Icon({
-                      alt: "Download",
-                      size: "1.3rem",
-                      src: "{personaleModel>stato}",
-                      color: "{personaleModel>color}"
-                  }),
-                  new Text({ text: "{personaleModel>data}" }),
-                  new Text({ text: "{personaleModel>esito_validazione}" }),
-                  new Input({
-                      textAlign: "Center",
-                      value: "{personaleModel>scadenza}",
-                      editable: "{= ${userModel>/tipoUtente} === 'Interno' ? false : ${personaleModel>editable} }"
-                  })
-              ];
-              case "car":
-                return [
-                  new Text({ text: "{mezziModel>descrizione}" }),
-                  new Button({
-                      visible: "{= !${mezziModel>scarica} ? false : true}",
-                      icon: "{mezziModel>scarica}",
-                      type: "Emphasized"
-                  }),
-                  new Select({
-                      width: "150px",
-                      items: { path: "mezziModel>validazione",templateShareable: false,
-                    template: new Item({
-                        key: "{value}",
-                        text: "{text}"
-                    })
-                  }
-                  }),
-                  new Button({
-                      type: "Emphasized",
-                      icon: "{mezziModel>allega}",
-                      press: "onOpenMaskAllega"
-                  }),
-                  new Icon({
-                      alt: "Download",
-                      size: "1.3rem",
-                      src: "{mezziModel>stato}",
-                      color: "{mezziModel>color}"
-                  }),
-                  new Text({ text: "{mezziModel>data}" }),
-                  new Text({ text: "{mezziModel>esito_validazione}" }),
-                  new Input({
-                      textAlign: "Center",
-                      value: "{mezziModel>scadenza}",
-                      editable: "{= ${userModel>/tipoUtente} === 'Interno' ? false : ${mezziModel>editable} }"
-                  })
-              ];
-              default:
-                  return [];
-          }
-        },
+          this.sKey = oEvent.getSource().getSelectedKey();
+          const oPanel = this.byId("panelContainer");
+          switch (this.sKey) {
+            case "info":
+                this.loadFragment("tableAzienda", oPanel);
+                oPanel.setVisible(true)
+                break;
+            case "people":
+                this.loadFragment("tablePersonale", oPanel);
+                oPanel.setVisible(true)
+                break;
+            case "car":
+                this.loadFragment("tableMezzi", oPanel);
+                oPanel.setVisible(true)
+                break;
+            default:
+                break;
+        }
+    },
+    
+    loadFragment: function(sFragmentName, oPanel) {
+        var oFragment = sap.ui.xmlfragment("portalefornitori.portalefornitori.view.fragment.documenti." + sFragmentName, this);
+        oPanel.removeAllContent();
+        oPanel.addContent(oFragment);
+    },
+        
+      //   createInfoTable:function () { 
+      //     const oTable = new sap.m.Table({
+      //       id: "changeTable",
+      //       sticky: ["HeaderToolbar", "ColumnHeaders"],
+      //       growing: true,
+      //       autoPopinMode: true,
+      //       class: "table",
+      //       items: {
+      //           path: "docModel>/",
+      //           factory: this.createCellContent("people",this.getOwnerComponent().getModel("userModel"))
+      //       }
+      //     });
+      
+      //     oTable.addColumn(new sap.m.Column({
+      //         width: "100px",
+      //         hAlign: "Left",
+      //         header: new Text({ text: "{i18n>Descrizione}" })
+      //     }));
+      //     oTable.addColumn(new sap.m.Column({
+      //         width: "60px",
+      //         hAlign: "Center",
+      //         header: new Text({ text: "{i18n>Scarica}" })
+      //     }));
+      //     oTable.addColumn(new sap.m.Column({
+      //         width: "100px",
+      //         hAlign: "Center",
+      //         visible: "{= ${userModel>/tipoUtente} === 'Interno' ? true : false }",
+      //         header: new Text({ text: "{i18n>Validazione}" })
+      //     }));
+      //     oTable.addColumn(new sap.m.Column({
+      //         width: "60px",
+      //         hAlign: "Center",
+      //         visible: "{= ${userModel>/tipoUtente} === 'Interno' ? false : true }",
+      //         header: new Text({ text: "{i18n>Allega}" })
+      //     }));
+      //     oTable.addColumn(new sap.m.Column({
+      //         width: "60px",
+      //         hAlign: "Center",
+      //         header: new Text({ text: "{i18n>Stato}" })
+      //     }));
+      //     oTable.addColumn(new sap.m.Column({
+      //         width: "130px",
+      //         hAlign: "Center",
+      //         visible: "{= ${userModel>/tipoUtente} === 'Interno' ? true : false }",
+      //         header: new Text({ text: "{i18n>Datacaricamento}" })
+      //     }));
+      //     oTable.addColumn(new sap.m.Column({
+      //         width: "60px",
+      //         hAlign: "Center",
+      //         visible: "{= ${userModel>/tipoUtente} === 'Interno' ? false : true }",
+      //         header: new Text({ text: "{i18n>Esito}" })
+      //     }));
+      //     oTable.addColumn(new sap.m.Column({
+      //         width: "90px",
+      //         hAlign: "Center",
+      //         header: new Text({ text: "{i18n>Scadenza}" })
+      //     }));  
+      //       return oTable;
+      //   },
+      //  createCellContent: function (sModelName, sTipoUtente) {
+      //   debugger
+      //     switch (sModelName) {
+      //         case "info":
+      //             return [
+      //                 new Text({ text: "{docModel>descrizione}" }),
+      //                 new Button({
+      //                     visible: "{= !${docModel>scarica} ? false : true}",
+      //                     icon: "{docModel>scarica}",
+      //                     type: "Emphasized"
+      //                 }),
+      //                 new Select({
+      //                     width: "150px",
+      //                     items: { path: "docModel>validazione", templateShareable: false,
+      //                     template: new Item({
+      //                       key: "{value}",
+      //                       text: "{text}"
+      //                   })
+      //                   }
+      //                 }),
+      //                 new Button({
+      //                     type: "Emphasized",
+      //                     icon: "{docModel>allega}",
+      //                     press: "onOpenMaskAllega"
+      //                 }),
+      //                 new Icon({
+      //                     alt: "Download",
+      //                     size: "1.3rem",
+      //                     src: "{docModel>stato}",
+      //                     color: "{docModel>color}"
+      //                 }),
+      //                 new Text({ text: "{docModel>data}" }),
+      //                 new Text({ text: "{docModel>esito_validazione}" }),
+      //                 new Input({
+      //                     textAlign: "Center",
+      //                     value: "{docModel>scadenza}",
+      //                     editable: "{= ${userModel>/tipoUtente} === 'Interno' ? false : ${docModel>editable} }"
+      //                 })
+      //             ];
+      //         case "people":
+      //           return [
+      //             new Text({ text: "{personaleModel>descrizione}" }),
+      //             new Button({
+      //                 visible: "{= !${personaleModel>scarica} ? false : true}",
+      //                 icon: "{personaleModel>scarica}",
+      //                 type: "Emphasized"
+      //             }),
+      //             new Select({
+      //                 width: "150px",
+      //                 items: { path: "personaleModel>validazione" , templateShareable: false,
+      //               template: new Item({
+      //                   key: "{value}",
+      //                   text: "{text}"
+      //               })
+      //             }
+      //             }),
+      //             new Button({
+      //                 type: "Emphasized",
+      //                 icon: "{personaleModel>allega}",
+      //                 press: "onOpenMaskAllega"
+      //             }),
+      //             new Icon({
+      //                 alt: "Download",
+      //                 size: "1.3rem",
+      //                 src: "{personaleModel>stato}",
+      //                 color: "{personaleModel>color}"
+      //             }),
+      //             new Text({ text: "{personaleModel>data}" }),
+      //             new Text({ text: "{personaleModel>esito_validazione}" }),
+      //             new Input({
+      //                 textAlign: "Center",
+      //                 value: "{personaleModel>scadenza}",
+      //                 editable: "{= ${userModel>/tipoUtente} === 'Interno' ? false : ${personaleModel>editable} }"
+      //             })
+      //         ];
+      //         case "car":
+      //           return [
+      //             new Text({ text: "{mezziModel>descrizione}" }),
+      //             new Button({
+      //                 visible: "{= !${mezziModel>scarica} ? false : true}",
+      //                 icon: "{mezziModel>scarica}",
+      //                 type: "Emphasized"
+      //             }),
+      //             new Select({
+      //                 width: "150px",
+      //                 items: { path: "mezziModel>validazione",templateShareable: false,
+      //               template: new Item({
+      //                   key: "{value}",
+      //                   text: "{text}"
+      //               })
+      //             }
+      //             }),
+      //             new Button({
+      //                 type: "Emphasized",
+      //                 icon: "{mezziModel>allega}",
+      //                 press: "onOpenMaskAllega"
+      //             }),
+      //             new Icon({
+      //                 alt: "Download",
+      //                 size: "1.3rem",
+      //                 src: "{mezziModel>stato}",
+      //                 color: "{mezziModel>color}"
+      //             }),
+      //             new Text({ text: "{mezziModel>data}" }),
+      //             new Text({ text: "{mezziModel>esito_validazione}" }),
+      //             new Input({
+      //                 textAlign: "Center",
+      //                 value: "{mezziModel>scadenza}",
+      //                 editable: "{= ${userModel>/tipoUtente} === 'Interno' ? false : ${mezziModel>editable} }"
+      //             })
+      //         ];
+      //         default:
+      //             return [];
+      //     }
+      //   },
       }
     );
   }
